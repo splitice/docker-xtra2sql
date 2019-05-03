b12740ad3caf5504316869ab3ea8ba807c53bb74
Mathew Heard <splitice@users.noreply.github.com>
2019-05-03 16:17:30 +1000

    ...

--- restore.sh
+++ restore.sh
@@ -5,11 +5,19 @@ set -x
 rm /backups/output/* -R -f
 cp /backups/base/ /backups/output/db -R
 if [[ -d /backups/increment ]]; then
-    xtrabackup --prepare --apply-log-only --target-dir=/backups/output
+    xtrabackup --prepare --apply-log-only --target-dir=/backups/output/db
     xtrabackup --prepare --target-dir=/backups/output/db --incremental-dir=/backups/increment
 else
     xtrabackup --prepare --target-dir=/backups/output/db
 fi
 
+echo "Starting MySQL"
+/usr/sbin/mysqld --datadir /backups/output/db &
+sleep 15
+
+mkdir -p /backups/output/sql
+mysqldump --tab=/backups/output/sql --all-databases
+
+echo "==[ Finished ]=="
 echo "SQL files are now in /backups/output/sql/table.sql"
 echo "Importable files are now in /backups/output/db"
