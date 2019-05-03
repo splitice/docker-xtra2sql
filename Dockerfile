c05ddc8b86e676775b890e149a1863d87f40377c
Mathew Heard <splitice@users.noreply.github.com>
2019-05-03 18:53:50 +1000

    ...

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM debian
 MAINTAINER SplitIce mheard@x4b.net
 
 RUN apt-get update; \
-    apt-get -y install wget lsb-release gpgv gnupg2; \
+    apt-get -y install wget lsb-release gpgv gnupg2 md5sum rsync; \
     wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb; \
     dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb; \
     percona-release enable-only tools release; \
--- restore.sh
+++ restore.sh
@@ -2,12 +2,33 @@
 
 set -x
 
-rm /backups/output/* -R -f
-cp /backups/base/ /backups/output/db -R
+if [[ -d /backups/output/db ]]; then
+    if [[ -f "/backups/output/db/xtrabackup_checkpoints "]]; then
+        if [[ $(md5sum /backups/base/xtrabackup_checkpoints | awk '{print $1}') == $(md5sum /backups/output/db/xtrabackup_checkpoints | awk '{print $1}') ]]; then
+            echo "Previous backup supplied"
+        else
+            rm /backups/output/db -R -f
+        fi
+    else
+        rm /backups/output/db -R -f
+    fi
+fi
+
+if [[ -d /backups/output/sql ]];
+    rm /backups/output/sql -R -f
+fi
+
+if [[ ! -f "/backups/output/db/xtrabackup_checkpoints "]]; then
+    cp /backups/base/ /backups/output/db -R
+fi
+
 if [[ -d /backups/increment ]]; then
+    xtrabackup --decompress --target-dir=/backups/output/db
+    xtrabackup --decompress --target-dir=/backups/output/increment
     xtrabackup --prepare --apply-log-only --target-dir=/backups/output/db
     xtrabackup --prepare --target-dir=/backups/output/db --incremental-dir=/backups/increment
 else
+    xtrabackup --decompress --target-dir=/backups/output/db
     xtrabackup --prepare --target-dir=/backups/output/db
 fi
 
