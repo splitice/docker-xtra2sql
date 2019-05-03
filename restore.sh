315f099d08d11189468989f408b0aaaffb8f2d6f
Mathew Heard <splitice@users.noreply.github.com>
2019-05-03 16:08:32 +1000

    ...

--- restore.sh
+++ restore.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-rm /backups/base/* -R -f
-cp /backups/base/ /backups/output -R
+set -x
+
+rm /backups/output/* -R -f
+cp /backups/base/ /backups/output/db -R
 if [[ -d /backups/increment ]]; then
     xtrabackup --prepare --apply-log-only --target-dir=/backups/output
-    xtrabackup --prepare --target-dir=/backups/output --incremental-dir=/backups/increment
+    xtrabackup --prepare --target-dir=/backups/output/db --incremental-dir=/backups/increment
 else
-    xtrabackup --prepare --target-dir=/backups/output
+    xtrabackup --prepare --target-dir=/backups/output/db
 fi
 
-echo "Backup is now in /backups/output"
+echo "SQL files are now in /backups/output/sql/table.sql"
+echo "Importable files are now in /backups/output/db"
